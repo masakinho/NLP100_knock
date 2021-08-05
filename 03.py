14f21655154b02f730e104781204a3eb8d08ff64
masakinho <masaki.7.ishi.826@gmail.com>
2021-08-05 17:45:14 +0900

    replaceで文字列削除 splitで分割 lenで長さ []でリスト作成

---
+++ 03.py
@@ -0,0 +1,12 @@
+import re
+
+str = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
+
+str1 = str.replace(',', '')
+str2 = str1.replace('.', '')
+
+list = str2.split()
+
+ans = [len(i) for i in list]
+
+print(ans)
