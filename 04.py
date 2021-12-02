684f0a1d8f04d62d60e711cf2046be1f06fcc646
masakinho <masaki.7.ishi.826@gmail.com>
2021-12-02 14:06:15 +0900

    renew

--- 04.py
+++ 04.py
@@ -6,10 +6,10 @@ list = str2.split()
 numlist = [1, 5, 6, 7, 8, 9, 15, 16, 19]
 ans = {}
 
-for i, key in enumerate(list):
+for i, word in enumerate(list):
     if i + 1 in numlist:
-        ans[key[:1]] = i + 1
+        ans[word[:1]] = i + 1
     else:
-        ans[key[:2]] = i + 1
+        ans[word[:2]] = i + 1
 
 print(ans)
